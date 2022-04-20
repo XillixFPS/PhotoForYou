a4ea059410324398582f0b06d16625575c57ff95
Ugo Monrocq <ugo.monrocq@gmail.com>
2022-04-20 11:01:16 +0200

    Gestion Erreur + .htaccess
    Gestion des Erreurs avec htaccess et j'ai fait en sorte que les utilisateurs ne peuvent pas accéder à des dossiers comme le dossier includes

--- includes/header.php
+++ includes/header.php
@@ -45,7 +45,7 @@ if(isset($_SESSION['login'])){
 <header>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container px-4 px-lg-5">
-        <a class="navbar-brand" href="index.php">PhotoForYou</a>
+        <a class="navbar-brand" href="index">PhotoForYou</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
 
     <?php
